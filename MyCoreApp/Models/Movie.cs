7639b1e65dca1dd2484d76aaa177946ceedc0487
Kirill Lagutin <lagutin.k@mail.ru>
2022-07-29 21:53:09 +0300

    Добавьте файлы проекта.

---
+++ MyCoreApp/Models/Movie.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyCoreApp.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; } = string.Empty;
+
+        [Display(Name ="Дата выпуска"), DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}",
+            ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
+        public string Genre { get; set; } = string.Empty;
+
+        [Range(100, 1000)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""\s-]*$")]
+        [Required]
+        [StringLength(5)]
+        public string Rating { get; set; } = string.Empty;
+
+        //[DataType.EmailAddress]
+        //[DataType.Date]
+        //[DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}",
+        //    ApplyFormatInEditMode = true)]
+
+        //[Range(typeof(DateTime), "1/1/1999", "1/1/2022")]
+    }
+}
---
+++ MyCoreApp/Models/SeedData.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCoreApp.Data;
+
+namespace MyCoreApp.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using var context = new MyCoreAppContext
+            (
+                serviceProvider.GetRequiredService<DbContextOptions<MyCoreAppContext>>()
+            );
+            
+            if(context == null || context.Movie == null)
+            {
+                throw new ArgumentNullException("Null MyAppCoreContext");
+            }
+
+            if(context.Movie.Any())
+            {
+                return;
+            }
+
+            context.Movie.AddRange
+            (
+                new Movie
+                {
+                    Title = "1",
+                    ReleaseDate = DateTime.Parse("1999-09-09"),
+                    Genre = "a",
+                    Price = 655.08M,
+                    Rating = "+18"
+                },
+                new Movie
+                {
+                    Title = "2",
+                    ReleaseDate = DateTime.Parse("1999-09-10"),
+                    Genre = "b",
+                    Price = 125.08M,
+                    Rating = "+16"
+                },
+                new Movie
+                {
+                    Title = "3",
+                    ReleaseDate = DateTime.Parse("1999-09-11"),
+                    Genre = "c",
+                    Price = 345.08M,
+                    Rating = "+16"
+                }
+            );
+
+            context.SaveChanges();
+        }
+    }
+}
